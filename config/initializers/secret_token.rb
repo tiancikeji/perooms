11e930c92b89f6b8609ed0b6cbcc8207484ab85d
vissul <gvissul@gmail.com>
2012-09-29 22:46:51 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perooms::Application.config.secret_token = '4255bd893630e7e396cb612b58e9b89445e48ea7f4f01ee86d220d8c30d5a8109aa88fe9042f76d65769b7b1de3cf7a1116f5e8c7922e0f5b66c7759ba21a10a'
