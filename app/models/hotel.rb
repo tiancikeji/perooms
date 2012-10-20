b4c17f568bb472fcb249bfc9d9f4fb12b130264c
vissul <vissul@vissul-MacBookPro.(none)>
2012-10-20 22:54:19 +0800

    change date

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -5,7 +5,7 @@ class Hotel < ActiveRecord::Base
 
   def self.getlist
     jsonArray = ""
-    uri = URI("http://api.ean.com/ean-services/rs/hotel/v3/list?minorRev=&cid=381126&apiKey=6bbj2j6dsw4gy74q2xkgshdc&locale=en_US&currencyCode=USD&city=Seattle&stateProvinceCode=WA&countryCode=US&supplierCacheTolerance=MED&arrivalDate=10/17/2012&departureDate=10/25/2012&room1=1,3&room2=1,5&supplierCacheTolerance=MED_ENHANCED")
+    uri = URI("http://api.ean.com/ean-services/rs/hotel/v3/list?minorRev=&cid=381126&apiKey=6bbj2j6dsw4gy74q2xkgshdc&locale=en_US&currencyCode=USD&city=Seattle&stateProvinceCode=WA&countryCode=US&supplierCacheTolerance=MED&arrivalDate=11/17/2012&departureDate=11/25/2012&room1=1,3&room2=1,5&supplierCacheTolerance=MED_ENHANCED")
     Net::HTTP.start(uri.host, uri.port,:use_ssl => uri.scheme == 'https') do |http|
       request = Net::HTTP::Get.new uri.request_uri
       response = http.request request
